d2596f2ea814d32b70ec1bca708e1a377eadc3d6
Mahvish Siddiqui <siddiquimahvish2@gmail.com>
2025-02-22 18:26:00 +0530

    folder structure changed

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, './uploads'))); 
-app.use(express.static(path.join(__dirname, '../gharpayy.com')));
-app.use("/fonts", express.static(path.join(__dirname, '../adminpages/fonts')));
-app.use("/css", express.static(path.join(__dirname, '../adminpages/css')));
-app.use("/js", express.static(path.join(__dirname, '../adminpages/js')));
-app.use("/libs", express.static(path.join(__dirname, '../adminpages/libs')));
+app.use(express.static(path.join(__dirname, './public')));
+app.use("/fonts", express.static(path.join(__dirname, './adminpages/fonts')));
+app.use("/css", express.static(path.join(__dirname, './adminpages/css')));
+app.use("/js", express.static(path.join(__dirname, './adminpages/js')));
+app.use("/libs", express.static(path.join(__dirname, './adminpages/libs')));
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -35,40 +35,40 @@ app.use("/api/users", userRoutes);
 app.use("/api/enquiry", enquiry);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'gharpayy.com', 'index.html'));
+  res.sendFile(path.join(__dirname, './public', 'index.html'));
 });
 
 app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "../adminpages", "login.html"));
+  res.sendFile(path.join(__dirname, "./adminpages", "login.html"));
 });
 
 app.get("/dashboard", authMiddleware, (req, res) => {
-  res.sendFile(path.join(__dirname, "../adminpages", "admin-index.html")); 
+  res.sendFile(path.join(__dirname, "./adminpages", "admin-index.html")); 
 });
 
 app.get("/addListing", authMiddleware, (req, res) => {
-  res.sendFile(path.join(__dirname, "../adminpages/addListing.html"));
+  res.sendFile(path.join(__dirname, "./adminpages/addListing.html"));
 });
 
 app.get("/allListings", authMiddleware, (req, res) => {
-  res.sendFile(path.join(__dirname, "../adminpages/allListings.html"));
+  res.sendFile(path.join(__dirname, "./adminpages/allListings.html"));
 });
 
 app.get("/enquirypage", authMiddleware, (req, res) => {
   
-  res.sendFile(path.join(__dirname, "../adminpages/enquiry.html")); 
+  res.sendFile(path.join(__dirname, "./adminpages/enquiry.html")); 
 });
 
 app.get("/profileSetting", (req, res) => {
-  res.sendFile(path.join(__dirname, "../adminpages/profile-setting.html"));
+  res.sendFile(path.join(__dirname, "./adminpages/profile-setting.html"));
 });
 
 app.get("/resetpass", (req, res) => {
-  res.sendFile(path.join(__dirname, "../adminpages/reset-password.html"));
+  res.sendFile(path.join(__dirname, "./adminpages/reset-password.html"));
 });
 
 app.get("/invoice.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "../adminpages/invoice.html"));
+  res.sendFile(path.join(__dirname, "./adminpages/invoice.html"));
 });
 
 app.listen(PORT, () => {
